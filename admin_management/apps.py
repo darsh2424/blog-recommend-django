a593939c3e5f62d55d2b48d2b303f4edf91c054a
Darsh Parikh <iamdarsh2424@gmail.com>
2025-10-13 09:14:24 +0530

    Emptying the branch

--- admin_management/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class AdminManagementConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'admin_management'
-
-    def ready(self):
-        from . import signals
--- users/templatetags/custom_filters.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from django.utils.html import strip_tags
-import html
-
-register = template.Library()
-
-@register.filter
-def clean_text(value):
-    return html.unescape(strip_tags(value))
