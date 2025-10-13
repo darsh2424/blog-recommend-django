7f666cbfd30796e41b6ea36bab0d8fb53a27894c
Darsh Parikh <iamdarsh2424@gmail.com>
2025-10-13 09:39:05 +0530

    changes

--- blog_recommend/settings.py
+++ blog_recommend/settings.py
@@ -86,11 +86,19 @@ WSGI_APPLICATION = 'blog_recommend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.parse(
+        os.environ.get('DATABASE_URL', f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}"),
+        conn_max_age=600,
+        ssl_require=False
+    )
 }
 
 # DATABASES = {
